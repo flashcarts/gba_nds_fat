ef34b2562ab5e0153d1404f5eab33e828b314900
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-11-28 23:39:19 -0800

    gba_nds_fat 2006-03-03

--- io_m3sd.h
+++ io_m3sd.h
@@ -1,8 +1,8 @@
 /*
-	io_m3cf.h 
+	io_m3sd.h  by SaTa.
 
-	Hardware Routines for reading a compact flash card
-	using the M3 CF
+	Hardware Routines for reading an SD card
+	using the M3 SD
 
 	This software is completely free. No warranty is provided.
 	If you use it, please give me credit and email me about your
@@ -11,15 +11,15 @@
 	See gba_nds_fat.txt for help and license details.
 */
 
-#ifndef IO_M3CF_H
-#define IO_M3CF_H
+#ifndef IO_M3SD_H
+#define IO_M3SD_H
 
-// 'M3CF'
-#define DEVICE_TYPE_M3CF 0x4643334D
+// 'M3SD'
+#define DEVICE_TYPE_M3SD 0x4453334D
 
 #include "disc_io.h"
 
 // export interface
-extern LPIO_INTERFACE M3CF_GetInterface(void) ;
+extern LPIO_INTERFACE M3SD_GetInterface(void) ;
 
-#endif	// define IO_M3CF_H
+#endif	// define IO_M3SD_H
